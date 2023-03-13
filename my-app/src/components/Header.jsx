e1fc0693ffd1b2d27426238c50f2534b147f2947
abdalla <abdalla.hagos@gmail.com>
2023-03-12 22:03:13 -0400

    new features

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -7,21 +7,23 @@ function Header(){
                 <div className='wrapper'>
                     <div className='flexHeader'>
                         <img className='logo' src="/logo-mobile.svg" alt="kanban logo" />
-                        <h1 className='primaryHeading hiddenLogo'>Kanban</h1>
+                        <h1 className='primaryHeading hidden--item'>Kanban</h1>
 
                         {/* p only visible on desktop version cuz it is not a button anymore */}
                         {/* <p>Platform Launch</p> */}
                         <button className='boardTitle' aria-expanded='false'>
                             Platform Launch
-                            <img className='platformLaunch__arrow' src="/icon-chevron-up.svg" alt="arrow icon" />
+                            <img className='boardTitle__arrow' src="/icon-chevron-up.svg" alt="arrow icon" />
                         </button>
                         
                         <button className='newTask'>
                             <img className='Plus--icon' src="/icon-add-task-mobile.svg" alt="plus sign icon" />
-                           <span>Add New Task</span>
+                           <span className='hidden--item'>Add New Task</span>
                         </button>
-
-                        <img className='ellipsisIcon' src="/icon-vertical-ellipsis.svg" alt="vertical ellipsis icon" />
+                        
+                        <button className='ellipsis'>
+                            <img className='ellipsis__Icon' src="/icon-vertical-ellipsis.svg" alt="vertical ellipsis icon" />
+                        </button>
                     </div>  
 
                     <div className='sideBar'>
